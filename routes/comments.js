b7404cbd34d63f6125ede35ff97a5d8c0c772ea3
Devon Marantz <DMarantz@talentpath.com>
2019-03-01 14:46:06 -0800

    Add lowdb

--- routes/comments.js
+++ routes/comments.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const shortid = require('shortid');
 const moment = require('moment');
+const lowdb = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 const commentData = require('../data');
+
+// create the db file if it doesn't exist
+// and seed it with data
+const adapter = new FileSync('db.json', {
+    defaultValue: { comments: commentData},
+});
+
+const db = lowdb(adapter);
 
 const router = express.Router();
 
 // get all comments
 router.get('/', (req,res) => {
-    res.json(commentData);
+    const comments = db.get('comments').value();
+    res.json(comments);
 });
 
 // get a single comment by id
 router.get('/:id', (req,res) => {
-    const myComment = commentData.find(comment => 
-        comment.id === parseInt(req.params.id));
+    const myComment = db.get('comments').find({id: req.params.id}).value();
     if(myComment){
         res.send(myComment);
     } else {
@@ -35,48 +45,44 @@ router.post('/', (req,res) => {
             timestamp: moment().format()
     };
     // add to comment data
-    commentData.push(newComment);
+    db.get('comments').push(newComment).write()
     // return all the comments (make sure the new comment is included!)
-    res.status(201).json({ msg: 'Comment Succesfully created', comments: commentData});
+    res.status(201).json({ msg: 'Comment Succesfully created', comments: db.get('comments').value()});
     // BONUS: if request has no body text (or text is empty) send proper error code and a message
 });
 
 // delete comment
 router.delete('/:id', (req,res) => {
-    // Get comment id
-    const myComment = commentData.find(comment => 
-        comment.id === parseInt(req.params.id));
-    const indexComment = commentData.indexOf(myComment);
-    if(myComment){
-        // Use comment id and delete comment
-        commentData.splice(indexComment, 1);
-
-        res.status(204).json({ msg: 'Comment Succesfully deleted', comments: commentData});
-    } else {
-        res.status(400).json({msg:'Bad Request'});
+    // check if the db has a comment with id of req.params.id
+    if(!db.get('comments')
+        .find({id: req.params.id})
+        .value()
+    ) {
+        return res.status(404).json({msg:'Invalid ID'});
     }
+
+    db.get('comments').remove({id: req.params.id}).write();
+
+    res.status(200).json({ msg: 'Comment Succesfully deleted', comments: db.get('comments').value()});
 });
 
 // update comment
 router.put('/:id', (req,res) => {
-    // Get comment id
-    const myComment = commentData.find(comment => 
-        comment.id === parseInt(req.params.id));
-    if(myComment){
-        
-        const indexComment = commentData.indexOf(myComment);
-        const newComment = {
-        text: req.body.text,
-        id: parseInt(req.params.id),
-        timestamp: moment().format()
-        };
-        // Use comment id and delete comment
-        commentData.splice(indexComment, 1, newComment);
-        // res.status(201).json({ msg: 'Comment Succesfully replaced', commentData});
-        res.json(commentData);
-    } else {
-        res.status(404).json({msg:'Invalid ID'});
+    if(!req.body.text) {
+        return res.status(400).json({msg: 'Invalid syntax'});
     }
+
+    // check if the db has a comment with id of req.params.id
+    if(!db.get('comments').find({id: req.params.id}).value()) {
+        return res.status(404).json({msg:'Invalid ID'});
+    }
+
+    db.get('comments')
+        .find({id: req.params.id})
+        .assign({text: req.body.text})
+        .write();
+
+    return res.json(db.get('comments').value());
 });
 
 module.exports = router;
